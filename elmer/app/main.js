2e6cfd7883784a49809d5a85808da105f742f2fa
worry <me@worry.name>
2012-12-17 15:16:07 -0800

    Initial elmer commit, ages late.

---
+++ elmer/app/main.js
@@ -0,0 +1,62 @@
+require([
+  // Application.
+  "app",
+
+  // jQuery.
+  "jquery",
+
+  // backbone.
+  "backbone",
+
+  // Main Router.
+  "router"
+],
+
+function(app, Router) {
+
+  // Define your master router on the application namespace and trigger all
+  // navigation from this instance.
+  app.router = new Router();
+
+  // Trigger the initial route and enable HTML5 History API support, set the
+  // root folder to '/' by default.  Change in app.js.
+  Backbone.history.start({ pushState: true, root: app.root });
+
+  // All navigation that is relative should be passed through the navigate
+  // method, to be processed by the router. If the link has a `data-bypass`
+  // attribute, bypass the delegation completely.
+  $(document).on("click", "a[href]:not([data-bypass])", function(evt) {
+    // Get the absolute anchor href.
+    var href = { prop: $(this).prop("href"), attr: $(this).attr("href") };
+    // Get the absolute root.
+    var root = location.protocol + "//" + location.host + app.root;
+
+    // Ensure the root is part of the anchor href, meaning it's relative.
+    if (href.prop.slice(0, root.length) === root) {
+      // Stop the default event to ensure the link will not cause a page
+      // refresh.
+      evt.preventDefault();
+
+      // `Backbone.history.navigate` is sufficient for all Routers and will
+      // trigger the correct events. The Router's internal `navigate` method
+      // calls this anyways.  The fragment is sliced from the root.
+      Backbone.history.navigate(href.attr, true);
+    }
+  });
+
+});
+
+
+/* Beginning backbone experiment. -- STRAIGHT RIP FROM ARCADE MAPS PROJECT - go get it from 2004_arcademaps*/
+/*
+Looking at putting this to use with json requests from /placemarks and /games
+Request structure shoud reflect: 
+  /placemarks/PLACEMARKID.json
+  /games/GAMEID.json
+Making sure that the contents of longer requests can be 
+put to use in rewriting another pane.  
+
+Just add elbow grease.
+*/
+
+/* Closures used to limit scope. */
